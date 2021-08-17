2886bf5fba57412843c54b221ab2691750ee2443
Maxim <maks96rush@yandex.ru>
2021-08-17 15:34:18 +0200

    init: deploying the task

---
+++ ReversingAString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReversingAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString("abcde"));
+        }
+
+        public static string ReverseString(string s)
+        {
+            if (s.Length <= 100)
+            {
+                char[] charArray = s.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
+            throw new Exception("The input string is too long!");
+        }
+    }
+}
